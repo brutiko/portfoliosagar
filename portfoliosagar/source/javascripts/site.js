8c79e8cfce7b4644bae5e30b77b6fe8e13d89c2d
BRUNA <brutiko@gmail.com>
2017-03-12 02:03:39 +0100

    Anchor top menu to scroll down slowly.

--- portfoliosagar/source/javascripts/site.js
+++ portfoliosagar/source/javascripts/site.js
@@ -1,4 +1,57 @@
-// This is where it all goes :)
+
+//SCROLL DOWN
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+//CHANGE ACTIVE STATE ON SCROLL
+var sections = $('section')
+  , nav = $('nav')
+  , nav_height = nav.outerHeight();
+ 
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+ 
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+ 
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+ 
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
+});
+
+
+//CHANGE ACTIVE STATE ON CLICK
+nav.find('a').on('click', function () {
+  var $el = $(this)
+    , id = $el.attr('href');
+ 
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 500);
+ 
+  return false;
+});
+
+
 //LOAD MORE
 // $(function(){
 //     $("div").slice(0, 10).show(); // select the first ten
