978041d34adf9871fc227e0b114c92028a034fcc
BRUNA <brutiko@gmail.com>
2017-03-14 15:32:47 +0100

    Added scroll down slowly + show more JS.

--- portfoliosagar/source/javascripts/site.js
+++ portfoliosagar/source/javascripts/site.js
@@ -1,97 +1,52 @@
-
 //SCROLL DOWN
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
-
-
-//CHANGE ACTIVE STATE ON SCROLL
-var sections = $('section')
-  , nav = $('nav')
-  , nav_height = nav.outerHeight();
- 
-$(window).on('scroll', function () {
-  var cur_pos = $(this).scrollTop();
- 
-  sections.each(function() {
-    var top = $(this).offset().top - nav_height,
-        bottom = top + $(this).outerHeight();
- 
-    if (cur_pos >= top && cur_pos <= bottom) {
-      nav.find('a').removeClass('active');
-      sections.removeClass('active');
- 
-      $(this).addClass('active');
-      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-    }
-  });
-});
-
-
-//CHANGE ACTIVE STATE ON CLICK
-nav.find('a').on('click', function () {
-  var $el = $(this)
-    , id = $el.attr('href');
- 
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  var target = $(this.hash);
+  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+  if (target.length) {
   $('html, body').animate({
-    scrollTop: $(id).offset().top - nav_height
-  }, 500);
- 
+  scrollTop: target.offset().top
+  }, 1000);
   return false;
+  }
+  }
+  });
 });
-
 
 //LOAD MORE
-// $(function(){
-//     $("div").slice(0, 10).show(); // select the first ten
-//     $("#load").click(function(e){ // click event for load more
-//         e.preventDefault();
-//         $("div:hidden").slice(0, 10).show(); // select next 10 hidden divs and show them
-//         if($("div:hidden").length == 0){ // check if any hidden divs still exist
-//             alert("No more divs"); // alert if there are none left
-//         }
-//     });
-//});
-
 $(function () {
-    $(".project-row").slice(0, 2).show();
-    $("#loadMore").on('click', function (e) {
-        e.preventDefault();
-        $(".project-row:hidden").slice(0, 2).slideDown();
-        if ($("project-row:hidden").length == 0) {
-            $("#load").fadeOut('slow');
-        }
-        $('html,body').animate({
-            scrollTop: $(this).offset().top
-        }, 1500);
-    });
+  $(".project-row").slice(0, 2).show();
+  $("#loadMore").on('click', function (e) {
+  e.preventDefault();
+  $(".project-row:hidden").slice(0, 2).slideDown();
+  if ($("project-row:hidden").length == 0) {
+  $("#load").fadeOut('slow');
+  }
+  $('html,body').animate({
+  scrollTop: $(this).offset().top
+  }, 1500);
+  });
 });
 
 $('a[href=#top]').click(function () {
-    $('body,html').animate({
-        scrollTop: 0
-    }, 600);
-    return false;
+  $('body,html').animate({
+  scrollTop: 0
+  }, 600);
+  return false;
 });
 
 $(window).scroll(function () {
-    if ($(this).scrollTop() > 50) {
-        $('.totop a').fadeIn();
-    } else {
-        $('.totop a').fadeOut();
-    }
+  if ($(this).scrollTop() > 50) {
+  $('.totop a').fadeIn();
+  } else {
+  $('.totop a').fadeOut();
+  }
 });
+
+
+
+
 
 
 
