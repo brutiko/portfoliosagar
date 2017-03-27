b07ec274520ff4625faa986ffe8023444aa986db
BRUNA <brutiko@gmail.com>
2017-03-27 19:04:28 +0200

    Add project layout to posts pages.

--- portfoliosagar/config.rb
+++ portfoliosagar/config.rb
@@ -12,6 +12,7 @@ page '/*.txt', layout: false
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
 page "/projects/*", :layout => "project"
+page "/posts/*", :layout => "project"
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
